cdd878b1e43b7a4749968c1fcd985bfa39306e24
Tanumay Dhar <tanumayd@kpit.com>
2019-03-24 19:23:46 +0530

    Abstraction

---
+++ JavaDemoframework/src/main/java/Bike.java
@@ -0,0 +1,24 @@
+
+abstract class Bike {
+	
+	abstract void run();
+	
+}
+class Honda extends Bike{
+	void run()
+	{
+		
+		System.out.println("Abstraction");
+	}
+}
+class Abstraction{
+	
+	public static void main(String[] args) {
+		
+		Honda h = new Honda();
+		h.run();
+		// TODO Auto-generated method stub
+
+	}
+
+}
