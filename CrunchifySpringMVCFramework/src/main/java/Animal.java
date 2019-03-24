639df78f3447dad3f737ba17be3f4b0e00d7a17e
Tanumay Dhar <tanumayd@kpit.com>
2019-03-24 18:14:42 +0530

    hello print

--- CrunchifySpringMVCFramework/src/main/java/Animal.java
+++ CrunchifySpringMVCFramework/src/main/java/Animal.java
@@ -1,13 +1,7 @@
-class Animal{  
-void eat(){System.out.println("eating...");}  
-}  
-class Dog extends Animal{  
-void bark(){System.out.println("barking...");}  
-}  
-class TestInheritance{  
-public static void main(String args[]){  
-Dog d=new Dog();  
-d.bark();  
-d.eat();
-//hello
-}}  
+class Animal{
+	
+	public static void main(String arg[]){
+		
+		System.out.println("java");
+	}
+}
